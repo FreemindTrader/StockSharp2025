e50e0d9fccbf6e5f1f64c9f544bd6b271cb0d4bc
Tony Lam <fxtraderhk@outlook.com>
2022-12-09 14:38:03 +0800

    Don't know why in FreemindSamples, some files are not seen.

--- Samples/Tutorials/Lesson05/01_HistoryEmulation/MainWindow.xaml.cs
+++ Samples/Tutorials/Lesson05/01_HistoryEmulation/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using StockSharp.Algo.Candles;
+using StockSharp.Algo.Storages;
+using StockSharp.Algo.Testing;
+using StockSharp.BusinessEntities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +24,47 @@ namespace _01_HistoryEmulation
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Security _security;
+        private Portfolio _portfolio;
+        private string _pathHistory = @"U:\ForexData";
+        private HistoryEmulationConnector _connector;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            CandleSettingsEditor.Settings = new StockSharp.Algo.Candles.CandleSeries()
+            {
+                CandleType = typeof( TimeFrameCandle ),
+                Arg = TimeSpan.FromMinutes( 5 )
+            };
+
+            DatePickerBegin.SelectedDate = new DateTime( 2020, 10, 1 );
+            DatePickerEnd.SelectedDate = new DateTime( 2020, 10, 30 );
+        }
+
+        private void Start_Click( object sender, RoutedEventArgs e )
+        {
+            _security = new Security 
+            {
+                Id = "SPX500@FXCM",
+                Code = "SPX500",
+                PriceStep = 1,
+                Board = ExchangeBoard.Fxcm
+            };
+
+            _portfolio = new Portfolio()
+            {
+                Name = "Test Account",
+                BeginValue = 150000
+            };
+
+            var storageRegistry = new StorageRegistry()
+            {
+                DefaultDrive = new LocalMarketDataDrive( _pathHistory )
+            };
+
+            _connector = new HistoryEmulationConnector( )
         }
     }
 }
