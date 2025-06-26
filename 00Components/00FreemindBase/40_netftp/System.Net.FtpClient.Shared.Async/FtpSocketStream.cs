7a2e13d0541f59f86d79b9521c67133d2409a743
FreemindTrader <112985010+FreemindTrader@users.noreply.github.com>
2025-06-26 23:22:38 +0800

    added netftp

---
+++ 00Components/00FreemindBase/40_netftp/System.Net.FtpClient.Shared.Async/FtpSocketStream.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace System.Net.FtpClient
+{
+    /// <summary>
+    /// Stream class used for talking. Used by FtpClient, extended by FtpDataStream
+    /// </summary>
+    public partial class FtpSocketStream
+    {
+        /// <summary>
+        /// Asynchronously accepts a connection from a listening socket
+        /// </summary>
+        /// <param name="callback"></param>
+        /// <param name="state"></param>
+        /// <returns></returns>
+        public IAsyncResult BeginAccept( AsyncCallback callback, object state )
+        {
+            if ( m_socket != null )
+                return m_socket.BeginAccept( callback, state );
+            return null;
+        }
+
+        /// <summary>
+        /// Completes a BeginAccept() operation
+        /// </summary>
+        /// <param name="ar">IAsyncResult returned from BeginAccept</param>
+        public void EndAccept( IAsyncResult ar )
+        {
+            if ( m_socket != null )
+            {
+                m_socket = m_socket.EndAccept( ar );
+                m_netStream = new NetworkStream( m_socket );
+            }
+        }
+    }
+}
---
+++ 00Components/00FreemindBase/40_netftp/System.Net.FtpClient.Shared/FtpExtensionAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+#if NET2
+///////
+// support for extension methods the 2.0 framework
+///////
+namespace System.Runtime.CompilerServices {
+    /// <summary>
+    /// support for extension methods the 2.0 framework
+    /// </summary>
+    public sealed class ExtensionAttribute : Attribute { }
+}
+#endif
